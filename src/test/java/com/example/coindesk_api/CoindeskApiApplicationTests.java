bbabb93a815f821783d451bd3b0dd8e0e5db669c
Chadwick <fcw11370@gmail.com>
2022-11-25 22:33:26 +0800

    初始化

---
+++ src/test/java/com/example/coindesk_api/CoindeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coindesk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoindeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
