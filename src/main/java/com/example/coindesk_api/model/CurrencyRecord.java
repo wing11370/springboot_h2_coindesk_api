36baf0eac4fd14713d579c197b2bcb891ee545df
Chadwick <fcw11370@gmail.com>
2022-11-27 20:59:13 +0800

    add CurrencyRecord model

---
+++ src/main/java/com/example/coindesk_api/model/CurrencyRecord.java
@@ -0,0 +1,11 @@
+package com.example.coindesk_api.model;
+
+public interface CurrencyRecord {
+
+    Integer getId();
+
+    String getDate();
+
+    double getRate();
+
+}
---
+++ src/main/java/com/example/coindesk_api/model/CurrencyRecordImpl.java
@@ -0,0 +1,7 @@
+package com.example.coindesk_api.model;
+
+public abstract class CurrencyRecordImpl implements CurrencyRecord {
+    Integer id;
+    String date;
+    Double rate;
+}
